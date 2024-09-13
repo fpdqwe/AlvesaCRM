de7a914d38f772f0d26d95933fdd768e113d0ac9
fpd.core20@gmail.com <fpd.core20@gmail.com>
2024-09-13 17:58:49 +0300

    ColorVM updates

--- DesktopUI/ViewModels/Products/ColorVM.cs
+++ DesktopUI/ViewModels/Products/ColorVM.cs
@@ -1,6 +1,7 @@
 ﻿using DesktopUI.Utilities;
 using Model = DesktopUI.Models.ProductModel;
 using Color = Domain.Entities.Product.ProductColor;
+using Size = Domain.Entities.Product.ProductSize;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
@@ -8,10 +9,26 @@ namespace DesktopUI.ViewModels.Products
 {
     public class ColorVM : BaseViewModel
     {
-
+        // Fields
         private Color _color;
+        private ObservableCollection<SizeVM> _sizes;
+        private SizeVM _selectedSize;
         private Model _model;
-        public Color Color
+        private int _index;
+		// Properties
+        public int Index
+        {
+            get => _index;
+            set
+            {
+                if (_index != value)
+                {
+                    _index = value;
+                    OnPropertyChanged(nameof(Index));
+                }
+            }
+        }
+		public Color Color
         {
             get => _color;
             set
@@ -19,23 +36,56 @@ namespace DesktopUI.ViewModels.Products
                 OnPropertyChanged(nameof(Color));
             }
         }
-
+        public ObservableCollection<SizeVM> Sizes
+        {
+            get => _sizes;
+            set
+            {
+                OnPropertyChanged(nameof(Sizes));
+            }
+        }
+        public SizeVM SelectedSize
+        {
+            get => _selectedSize;
+            set
+            {
+                _selectedSize = value;
+                OnPropertyChanged(nameof(SelectedSize));
+            }
+        }
+        // Ctors
         public ColorVM(Color color)
         {
             _color = color;
+            _sizes = GetSizeCollection(color.Sizes);
             _model = new Model();
         }
 
+        // Commands
 		public ICommand UpdateColorCommand { get; set; }
 		public ICommand DeleteColorCommand { get; set; }
 
-		public async void UpdateColor(object parameter)
+        // Commands realization
+		private async void UpdateColor(object parameter)
 		{
 			await _model.UpdateColor(Color);
 		}
-		public async void DeleteColor(object parameter)
+		private async void DeleteColor(object parameter)
 		{
 			await _model.DeleteColor(Color);
+		}
+
+		// Private methods and utilities
+		private ObservableCollection<SizeVM> GetSizeCollection(List<Size> sizes)
+		{
+			var result = new ObservableCollection<SizeVM>();
+            int i = 1;
+			foreach (var size in sizes)
+			{
+				result.Add(new SizeVM(size, _model, i));
+                i++;
+			}
+			return result;
 		}
 	}
 }
--- DesktopUI/ViewModels/Products/SizeVM.cs
+++ DesktopUI/ViewModels/Products/SizeVM.cs
@@ -1,12 +1,28 @@
 ﻿using DesktopUI.Utilities;
 using Domain.Entities.Product;
+using System.Windows.Input;
+using Model = DesktopUI.Models.ProductModel;
 
 namespace DesktopUI.ViewModels.Products
 {
 	public class SizeVM : BaseViewModel
 	{
 		private ProductSize _size;
-		private ProductModel _model;
+		private Model _model;
+		private int _index;
+		// Properties
+		public int Index
+		{
+			get => _index;
+			set
+			{
+				if (_index != value)
+				{
+					_index = value;
+					OnPropertyChanged(nameof(Index));
+				}
+			}
+		}
 		public ProductSize Size
 		{
 			get => _size;
@@ -16,12 +32,26 @@ namespace DesktopUI.ViewModels.Products
 				OnPropertyChanged(nameof(Size));
 			}
 		}
-
-        public SizeVM(ProductSize size)
+		// Ctors
+        public SizeVM(ProductSize size, Model model, int i)
         {
+			_index = i;
 			_size = size;
+			_model = model;
         }
 
-
-    }
+		// Commands
+		public ICommand UpdateSizeCommand { get; set; }
+		public ICommand DeleteSizeCommand { get; set; }
+		// Commands realization
+		public async void UpdateSize(object parameter)
+		{
+			await _model.UpdateSize(Size);
+		}
+		public async void DeleteSize(object parameter)
+		{
+			await _model.DeleteSize(Size);
+		}
+		// Private methods
+	}
 }
